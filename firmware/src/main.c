c597427ca291358374ffa20a542ca6b32c9c432a
Vacbo <peportella11@gmail.com>
2023-08-31 19:23:19 -0300

    compilation bug fixed

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,7 +1,7 @@
-#include "asf.h"
+#include <asf.h>
 
-#include "gfx_mono_text.h"
 #include "gfx_mono_ug_2832hsweg04.h"
+#include "gfx_mono_text.h"
 #include "sysfont.h"
 
 #define START_BUT_PIO PIOA
@@ -13,7 +13,7 @@
 #define PAUSE_BUT_PIN_MASK (1 << PAUSE_BUT_PIN)
 
 #define BUZ_PIN_PIO PIOA
-#define BUZ_PIN 4
+#define BUZ_PIN 3
 #define BUZ_PIN_MASK (1 << BUZ_PIN)
 
 #define NOTE_B0 31
@@ -40,77 +40,86 @@
 #define TEMPO 200
 
 typedef struct {
-    uint16_t pitch;
-    uint16_t beats;
-    uint16_t duration;
+	uint16_t pitch;
+	uint16_t beats;
+	uint16_t duration;
 } note;
 
 typedef struct {
-    note **notes;
-    uint32_t size;
-    uint16_t tempo;
+	note **notes;
+	uint32_t size;
+	uint16_t tempo;
 } song;
 
 note *createNote(uint16_t tempo, uint16_t pitch, uint16_t beats) {
-    note *note = malloc(sizeof(note));
+	note *note = malloc(sizeof(note));
 
-    note->pitch = pitch;
-    note->beats = beats;
-    note->duration = tempo * 60000 / beats;
+	note->pitch = pitch;
+	note->beats = beats;
+	note->duration = tempo * 60000 / beats;
 
-    if (note->beats < 0) {
-        note->duration *= 1.5;
-    }
-    return note;
+	if (note->beats < 0) {
+		note->duration *= 1.5;
+	}
+	return note;
 }
 
 song *createSong(note **notes, uint32_t size, uint16_t tempo) {
-    song *song = malloc(sizeof(song));
+	song *song = malloc(sizeof(song));
 
-    song->notes = notes;
-    song->size = size;
-    song->tempo = tempo;
+	song->notes = notes;
+	song->size = size;
+	song->tempo = tempo;
 
-    return song;
+	return song;
 }
 
 void playNextNote(note *note) {
-    for (int i = 0; i < note->duration; i++) {
-        pio_set(BUZ_PIN_PIO, BUZ_PIN_MASK);
-        delay_us(2 / note->pitch);
-        pio_clear(BUZ_PIN_PIO, BUZ_PIN_MASK);
-        delay_us(2 / note->pitch);
-    }
+	for (int i = 0; i < note->duration; i++) {
+		pio_set(BUZ_PIN_PIO, BUZ_PIN_MASK);
+		delay_us(2 / note->pitch);
+		pio_clear(BUZ_PIN_PIO, BUZ_PIN_MASK);
+		delay_us(2 / note->pitch);
+	}
 }
 
-int main(void) {
-    board_init();
-    sysclk_init();
-    delay_init();
+int main (void)
+{
+	board_init();
+	sysclk_init();
+	delay_init();
 
-    // Init OLED
-    gfx_mono_ssd1306_init();
+	// Disativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	// Habilida clock do periferico PIO 
+	pmc_enable_periph_clk(BUZ_PIN_PIO);
 
-    // Escreve na tela um circulo e um texto
-    gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-    gfx_mono_draw_string("mundo", 50, 16, &sysfont);
+	// Inicializa Buzzer como saida
+	pio_set_output(BUZ_PIN_PIO, BUZ_PIN_MASK, 0, 0, 0);
+	
+	// Init OLED
+	gfx_mono_ssd1306_init();
+	
+	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
+	gfx_mono_draw_string("mundo", 50,16, &sysfont);
 
-    /* Insert application code here, after the board has been initialized. */
-    while (1) {
+  /* Insert application code here, after the board has been initialized. */
+	while(1) {
 
-        int marioNotes[] = {NOTE_E5, NOTE_E5, REST, NOTE_E5, REST, NOTE_C5, NOTE_E5, NOTE_G5, REST, NOTE_G4, REST, NOTE_C5, NOTE_G4, REST, NOTE_E4, REST, NOTE_A4, NOTE_B4, NOTE_AS4, NOTE_A4};
-        int marioBeats[] = {8, 8, 8, 8, 8, 8, 8, 4, 4, 8, 4, -4, 8, 4, -4, 4, 4, 8, 4};
+		int marioNotes[] = {NOTE_E5, NOTE_E5, REST, NOTE_E5, REST, NOTE_C5, NOTE_E5, NOTE_G5, REST, NOTE_G4, REST, NOTE_C5, NOTE_G4, REST, NOTE_E4, REST, NOTE_A4, NOTE_B4, NOTE_AS4, NOTE_A4};
+		int marioBeats[] = {8, 8, 8, 8, 8, 8, 8, 4, 4, 8, 4, -4, 8, 4, -4, 4, 4, 8, 4};
 
-        note *notes[19];
+		note *notes[19];
 
-        for (int i = 0; i < 19; i++) {
-            note *note = createNote(TEMPO, marioNotes[i], marioBeats[i]);
-            notes[i] = note;
-        }
-        song *mario = createSong(notes, 19, TEMPO);
+		for (int i = 0; i < 19; i++) {
+			note *note = createNote(TEMPO, marioNotes[i], marioBeats[i]);
+			notes[i] = note;
+		}
+		song *mario = createSong(notes, 19, TEMPO);
 
-        for (uint32_t i = 0; i < mario->size; i++) {
-            playNextNote(mario->notes[i]);
-        }
-    }
+		for (uint32_t i = 0; i < mario->size; i++) {
+			playNextNote(mario->notes[i]);
+		}		
+	}
 }
